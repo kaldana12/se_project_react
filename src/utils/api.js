4a05c2e62f17947fcc1283db5eaf4bcaad7aa596
KAY <karen.aldana012@gmail.com>
2025-08-25 08:19:34 -0400

    updated api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "put the URL for your deployed backend here, including https://"
+    ? "https://https://www.wtwrkproject.jumpingcrab.com"
     : "http://localhost:3001";
 
 export function checkResponse(res) {
